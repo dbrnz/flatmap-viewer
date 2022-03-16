b91e4e3d3a5078c0285e326c65b0da2d42878658
David Brooks <dave@bcs.co.nz>
2022-03-17 11:27:12 +1300

    Don't highlight feature in info mode if it doesn't have a tooltip (e.g. the outline of the body).

--- src/interactions.js
+++ src/interactions.js
@@ -729,7 +729,9 @@ export class UserInteractions
         let info = '';
         let tooltip = '';
         if (displayInfo) {
-            this.activateFeature_(features[0]);
+            if (!'tooltip' in features[0].properties) {
+                this.activateFeature_(features[0]);
+            }
             info = this._infoControl.featureInformation(features, event.lngLat);
         }
         const lineFeatures = features.filter(feature => ('type' in feature.properties
