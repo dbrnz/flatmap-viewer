7a7eea727b7efd0d88d62b4b677c75eb68bda352
David Brooks <d.brooks@auckland.ac.nz>
2024-02-07 15:09:15 +1300

    Update copyright year.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Flatmap Viewer'
-copyright = '2019-2023, David Brooks'
+copyright = '2019-2024, David Brooks'
 author = 'David Brooks'
 
 # -- General configuration ---------------------------------------------------
