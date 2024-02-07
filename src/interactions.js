11d05dbd9120da6566eb6f697eea3d05b4584408
David Brooks <d.brooks@auckland.ac.nz>
2024-02-07 13:22:23 +1300

    Remove our original annotation tool -- apart from drawing regions on maps, all user dialogs and interaction with an annotation service are now external to the viewer.

--- src/interactions.js
+++ src/interactions.js
@@ -33,7 +33,6 @@ import polylabel from 'polylabel';
 
 //==============================================================================
 
-import {Annotator} from './annotation';
 import {LayerManager} from './layers';
 import {PATHWAYS_LAYER, PathManager} from './pathways';
 import {VECTOR_TILES_SOURCE} from './styling';
@@ -182,13 +181,6 @@ export class UserInteractions
             this.enableCentrelines(this.__pathManager.enabledCentrelines, true);
         }
 
-        // Add annotation capability
-        if (flatmap.options.annotator) {
-            this.__setupAnnotation();
-        } else {
-            this.__annotator = null;
-        }
-
         // Note features that are FC systems
         this.__systemsManager = new SystemsManager(this._flatmap, this, featuresEnabled);
 
@@ -311,41 +303,6 @@ export class UserInteractions
         }
         if (Object.keys(options).length > 0) {
             this._map.jumpTo(options);
-        }
-    }
-
-    async __setupAnnotation()
-    //=======================
-    {
-        // Add annotation capability
-
-        this.__annotator = new Annotator(this._flatmap, this);
-        const annotated_features = await this.__annotator.annotated_features();
-
-        // Flag features that have annotations
-        this.__featureIdToMapId = new Map();
-        for (const [mapId, ann] of this._flatmap.annotations) {
-            this.__featureIdToMapId.set(ann.id, mapId);
-            const feature = this.mapFeature(mapId);
-            if (feature !== undefined) {
-                this._map.setFeatureState(feature, { 'map-annotation': true });
-                if (annotated_features.includes(ann.id)) {
-                    this._map.setFeatureState(feature, { 'annotated': true });
-                }
-            }
-        }
-    }
-
-    setFeatureAnnotated(featureId)
-    //============================
-    {
-        if (this.__annotator) {
-            // featureId v's geoJSON id
-            const mapId = this.__featureIdToMapId.get(featureId);
-            const feature = this.mapFeature(mapId);
-            if (feature !== undefined) {
-                this._map.setFeatureState(feature, { 'annotated': true });
-            }
         }
     }
 
@@ -1039,28 +996,6 @@ export class UserInteractions
         }
     }
 
-    __annotationEvent(features)
-    //=========================
-    {
-        if (!this.__annotator) {
-            return;
-        }
-
-        event.preventDefault();
-
-        // Remove any tooltip
-        this.removeTooltip_();
-
-        // Don't respond to mouse events while the dialog is open
-        this.setModal_();
-
-        // The annotation dialog...
-        this.__annotator.annotate(features, () => {
-            this.unselectFeatures();
-            this.__clearModal();
-        });
-    }
-
     clickEvent_(event)
     //================
     {
@@ -1075,13 +1010,8 @@ export class UserInteractions
             this.unselectFeatures();
             return;
         }
-        const originalEvent = event.originalEvent;
-        if (originalEvent.altKey) {
-            this.__annotationEvent(clickedFeatures);
-            return;
-        }
         const clickedFeature = clickedFeatures[0];
-        this.selectionEvent_(originalEvent, clickedFeature);
+        this.selectionEvent_(event.originalEvent, clickedFeature);
         if (this._modal) {
             // Remove tooltip, reset active features, etc
             this.__resetFeatureDisplay();
