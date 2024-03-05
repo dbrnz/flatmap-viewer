d3b0ccd314904c195bf282fb1712954713e11949
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-03-06 11:11:10 +1300

    Add `action` into `updated` callback to show the update type

--- src/controls/annotation.js
+++ src/controls/annotation.js
@@ -190,6 +190,8 @@ export class AnnotationDrawControl
     //===================
     {
         const feature = this.#cleanFeature(event)
+        // specify updated callback type, either `move` or `change_coordinates`
+        feature.action = event.action
         if (feature) {
             if (this.__uncommittedFeatureIds.has(feature.id)) {
                 // Ignore updates on an uncommitted create or update
