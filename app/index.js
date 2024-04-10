265b0d0867f4b752b60644c857b4a0ffc4a82dd3
David Brooks <d.brooks@auckland.ac.nz>
2024-04-10 12:53:17 +1200

    Update comment re `devel` map server URL.

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ const MINIMAP = false; // { width: '10%', background: '#FCC' };
 
 //const MAP_ENDPOINT = 'https://mapcore-demo.org/flatmaps/';
 const MAP_ENDPOINT = 'http:localhost:8000/';
-//const MAP_ENDPOINT = 'https://mapcore-demo.org/devel/flatmap/v1/';
+//const MAP_ENDPOINT = 'https://mapcore-demo.org/devel/flatmap/v4/';   // v4 == devel
 
 
 window.onload = standaloneViewer(MAP_ENDPOINT, {
