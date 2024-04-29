e0ed3629e684af885e7ef22f5c3900e20600e870
David Brooks <d.brooks@auckland.ac.nz>
2024-04-29 15:24:43 +1200

    Build release as a distributable library.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { glob } from 'glob'
 import path from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { fileURLToPath } from 'node:url'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 export default defineConfig({
@@ -14,27 +12,15 @@ export default defineConfig({
     copyPublicDir: false,
     cssCodeSplit: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'FlatmapViewer',
-      fileName: 'flatmapviewer',
-      formats: ['es']
+      formats: ['es'],
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      name: 'FlatmapViewer'
     },
     rollupOptions: {
-      input: Object.fromEntries(
-        // https://rollupjs.org/configuration-options/#input
-        glob.sync('lib/**/*.{js,ts}').map(file => [
-          // 1. The name of the entry point
-          // lib/nested/foo.js becomes nested/foo
-          path.relative(
-            'lib',
-            file.slice(0, file.length - path.extname(file).length)
-          ),
-          // 2. The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
-      ),
       output: {
+        // Put chunk files at <output>/chunks
+        chunkFileNames: 'chunks/[name].[hash].js',
+        // Put chunk styles at <output>/assets
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
       }
