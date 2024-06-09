d7438562713afee27d712fd1b5d027a20708c854
David Brooks <d.brooks@auckland.ac.nz>
2024-06-09 21:01:43 +1200

    Tweak comment.

--- src/layers/acluster.ts
+++ src/layers/acluster.ts
@@ -187,8 +187,8 @@ export class ClusteredAnatomicalMarkerLayer
                     termToMarkerPoints.set(datasetMarker.term, markerPoints)
                 }
                 const markerPoint = termToMarkerPoints.get(datasetMarker.term)[0]
-                // We only need to update these property fields once all of the dataset's markers
-                // refer to the same two variables
+                // We only need to update these property fields once, as all of the dataset's markers
+                // refer to the same two property variables
                 const zoomCount = markerPoint.properties['zoom-count']
                 for (let zoom = 0; zoom <= this.#maxZoom; zoom += 1) {
                     if (datasetMarker.minZoom <= zoom && zoom < datasetMarker.maxZoom) {
