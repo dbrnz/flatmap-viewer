738e80640ff71191a410d3e1f945e069a39e847c
David Brooks <d.brooks@auckland.ac.nz>
2024-10-22 08:37:24 +1300

    Use a script to create a new release (on GitHub) and publish it (to npm).

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git status | grep -q "nothing to commit"
+dirty=($? != 0)
+if (( dirty )); then
+    git stash -u
+fi
+
+git push upstream
+git push upstream v$1
+gh release create upstream v$1 --verify-tag --title "Release $1" --notes ""
+
+npm publish
+
+if (( dirty )); then
+    git stash pop --quiet
+fi
