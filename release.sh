99ab963a823be2a2e5eb70aa9c689a303c4d584b
David Brooks <d.brooks@auckland.ac.nz>
2024-11-26 20:51:48 +1300

    We've renamed out Git remotes and now release to `origin`.

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ if (( dirty )); then
     git stash -u
 fi
 
-git push upstream
-git push upstream v$1
+git push origin
+git push origin v$1
 gh release create v$1 --verify-tag --title "Release $1" --notes ""
 
 npm publish
