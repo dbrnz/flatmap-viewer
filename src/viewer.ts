cef33742cf2730b95cc18dee65366a82749c18d3
David Brooks <dbrnz@users.noreply.github.com>
2026-04-23 13:35:19 +1200

    Release as version `4.6.4`.

--- src/viewer.ts
+++ src/viewer.ts
@@ -34,7 +34,7 @@ import * as utils from './utils'
 //==============================================================================
 
 // The released version of the viewer
-export const VIEWER_VERSION = '4.6.3'
+export const VIEWER_VERSION = '4.6.4'
 
 //==============================================================================
 
